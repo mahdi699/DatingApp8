9d9922a96438ba69e3e8e3720779e14bf74d04e6
Mahdi Haddad <haddadmahdi150@gmail.com>
2024-05-26 21:52:12 +0100

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -12,9 +12,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 //configure the http request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
